29fcb41ec28d382accdf46ecdc3ef3c86d8dc1cf
Sandy Maguire <sandy@sandymaguire.me>
2014-12-27 20:11:57 -0500

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,2 @@
+I'm calculating a bunch of end-of-year analytics of my life. This repo is some tools I wrote to help.
+
---
+++ facebook-messages.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python2.6
+import os.path
+import json
+import urllib2
+import urllib
+import urlparse
+import BaseHTTPServer
+import webbrowser
+import time
+import datetime
+from collections import defaultdict
+
+
+NAME = "tino"
+
+APP_ID = "1512576815698643"
+APP_SECRET = "6b61bba817c1fc44e3fb2696da004119"
+ENDPOINT = 'graph.facebook.com'
+REDIRECT_URI = 'http://localhost:8080/'
+ACCESS_TOKEN = None
+LOCAL_FILE = '.fb_access_token'
+YEAR = 2014
+
+def get_url(path, args=None):
+    args = args or {}
+    if ACCESS_TOKEN:
+        args['access_token'] = ACCESS_TOKEN
+    if 'access_token' in args or 'client_secret' in args:
+        endpoint = "https://"+ENDPOINT
+    else:
+        endpoint = "http://"+ENDPOINT
+    return endpoint+path+'?'+urllib.urlencode(args)
+
+def get(path, args=None):
+    print(get_url(path, args=args))
+    return urllib2.urlopen(get_url(path, args=args)).read()
+
+def getPage(path):
+    return urllib2.urlopen(path).read()
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        global ACCESS_TOKEN
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        code = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('code')
+        code = code[0] if code else None
+        if code is None:
+            self.wfile.write("Sorry, authentication failed.")
+            sys.exit(1)
+        response = get('/oauth/access_token', {'client_id':APP_ID,
+                                               'redirect_uri':REDIRECT_URI,
+                                               'client_secret':APP_SECRET,
+                                               'code':code})
+        ACCESS_TOKEN = urlparse.parse_qs(response)['access_token'][0]
+        open(LOCAL_FILE,'w').write(ACCESS_TOKEN)
+        self.wfile.write("You have successfully logged in to facebook. "
+                         "You can close this window now.")
+
+
+send = defaultdict(lambda: 0)
+recv = defaultdict(lambda: 0)
+totalSends = 0
+totalRecvs = 0
+def msgSent(convo, who):
+    global totalSends, totalRecvs
+    if convo == who:
+        recv[convo] += 1
+        totalRecvs += 1
+    else:
+        send[convo] += 1
+        totalSends += 1
+
+def handleComments(convo, data):
+    print "----next page: %s----" % convo
+    for item in data["data"]:
+        msgSent(convo, item["from"]["name"])
+    sleep(1)
+    if data["paging"]["next"]:
+        handleComments(convo, json.loads(getPage(data["paging"]["next"])))
+
+def handleStream(stream):
+    global NAME
+    convo = stream["to"]["data"]
+    for person in convo:
+        if person["name"] != NAME:
+            convo = person["name"]
+            break
+
+    handleComments(convo, stream["comments"])
+
+
+
+if __name__ == '__main__':
+    if not os.path.exists(LOCAL_FILE):
+        print "Logging you in to facebook..."
+        webbrowser.open(get_url('/oauth/authorize',
+                                {'client_id':APP_ID,
+                                 'redirect_uri':REDIRECT_URI,
+                                 'scope':'read_inbox'}))
+
+        httpd = BaseHTTPServer.HTTPServer(('127.0.0.1', 8080), RequestHandler)
+        while ACCESS_TOKEN is None:
+            httpd.handle_request()
+    else:
+        ACCESS_TOKEN = open(LOCAL_FILE).read()
+
+
+    timestamp = int(time.mktime(datetime.datetime.strptime("01/01/%d" % YEAR, "%d/%m/%Y").timetuple()))
+    try:
+        data = json.loads(get('/me/inbox', {"fields": 'comments.since(%d){from},to' % timestamp }))
+        for stream in data["data"]:
+            handleStream(stream)
+    except:
+        pass
+    finally:
+        print(repr(send))
+        print(repr(recv))
+        print("total sends: %d" % totalSends)
+        print("total recvs: %d" % totalRecvs)
